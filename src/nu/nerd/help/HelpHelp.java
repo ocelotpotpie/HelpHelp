6d215f9e12bc563cd126e3059626d433dae57779
totemo <TotemoResearch@gmail.com>
2017-02-20 23:11:36 +1030

    Implement /help-dump, /help-reload and /help-load <url> commands.

--- src/nu/nerd/help/HelpHelp.java
+++ src/nu/nerd/help/HelpHelp.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.util.Collection;
 
 import org.bukkit.Bukkit;
@@ -11,6 +13,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.help.HelpMap;
 import org.bukkit.help.HelpTopic;
@@ -24,13 +27,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class HelpHelp extends JavaPlugin {
     // ------------------------------------------------------------------------
     /**
-     * Insert custom help topics and programmatically generated indices
-     * according to the configuration.
+     * Plugin initialisation.
      */
     @Override
     public void onEnable() {
         saveDefaultConfig();
-        // TODO: insert topics.
     }
 
     // ------------------------------------------------------------------------
@@ -40,28 +41,43 @@ public class HelpHelp extends JavaPlugin {
      */
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (command.getName().equals("helphelp")) {
-            if (args.length == 1 && args[0].equals("reload")) {
-                reloadHelp();
-                // TODO: Implement configuration reloading.
-
-                return true;
-            } else if (args.length == 1 && args[0].equals("dump")) {
+        if (command.getName().equals("help-dump")) {
+            if (args.length == 0) {
                 dumpTopics(sender);
                 return true;
+            } else {
+                return false;
+            }
+
+        } else if (command.getName().equals("help-reload")) {
+            if (args.length == 0) {
+                reloadHelp(sender);
+                // TODO: Implement configuration reloading.
+                return true;
+            } else {
+                return false;
+            }
+        } else if (command.getName().equals("help-load")) {
+            if (args.length == 1) {
+                loadHelp(sender, args[0]);
+                return true;
+            } else {
+                return false;
             }
         }
-        return false;
+        return true;
     }
 
     // ------------------------------------------------------------------------
     /**
      * Reload "help.yml".
-     * 
+     *
      * This method uses reflection to access version-specific CraftBukkit
      * methods that probably won't go away.
+     *
+     * @param sender the command sender.
      */
-    protected void reloadHelp() {
+    protected void reloadHelp(CommandSender sender) {
         HelpMap helpMap = Bukkit.getServer().getHelpMap();
         Class<? extends HelpMap> simpleHelpMapClass = helpMap.getClass();
         try {
@@ -71,8 +87,38 @@ public class HelpHelp extends JavaPlugin {
             clear.invoke(helpMap);
             initializeGeneralTopics.invoke(helpMap);
             initializeCommands.invoke(helpMap);
+            sender.sendMessage(ChatColor.GOLD + "Help reloaded.");
         } catch (Exception ex) {
-            getLogger().severe("Unable to reload help: " + ex.toString());
+            sender.sendMessage(ChatColor.RED + "Unable to reload help: " + ex.toString());
+        }
+    }
+
+    // ------------------------------------------------------------------------
+    /**
+     * Load help from the specified URI.
+     *
+     * @param uri the URI as a string; generally a remote file URL.
+     */
+    protected void loadHelp(CommandSender sender, String uri) {
+        try {
+            MessageSink sink = s -> sender.sendMessage(ChatColor.RED + s);
+            HelpLoader loader = new HelpLoader();
+            loader.loadURI(uri, sink);
+
+            YamlConfiguration output = new YamlConfiguration();
+            HelpRenderer help = new HelpRenderer();
+            help.renderBoilerPlate(output, getConfig());
+            help.renderIndexTopics(output, loader, sink);
+            help.renderGeneralTopics(output, loader, sink);
+
+            output.save(new File("help.yml"));
+            reloadHelp(sender);
+        } catch (URISyntaxException | MalformedURLException ex) {
+            sender.sendMessage(ChatColor.RED + "Error loading help: malformed URL: " + ex.getMessage());
+        } catch (InvalidConfigurationException ex) {
+            sender.sendMessage(ChatColor.RED + "Error loading help: the YAML substitution section is malformed: " + ex.getMessage());
+        } catch (IllegalArgumentException | IOException ex) {
+            sender.sendMessage(ChatColor.RED + "Error loading help: " + ex.getMessage());
         }
     }
 
